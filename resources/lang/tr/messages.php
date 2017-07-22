38eb76c8b43c2ac101ff7b30df6f4d1c1e0dc5df
Enes Çakır <enes@cakir.web.tr>
2017-07-23 00:20:26 +0300

    Add update and delete methods to blood

--- resources/lang/tr/messages.php
+++ resources/lang/tr/messages.php
@@ -4,6 +4,7 @@ return [
 
     'blood' => [
         'create' => '<strong>:mobile</strong> numaralı bağışçı başarıyla sisteme kaydedildi.',
+        'update' => '<strong>:mobile</strong> numaralı bağışçı başarıyla güncellendi.',
     ],
 
 ];
