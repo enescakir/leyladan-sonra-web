a40afca59b1094fba4efc28b44e61f439c5f4f5d
Enes Çakır <enes@cakir.web.tr>
2020-01-18 23:13:46 +0300

    add maintanence page

---
+++ resources/views/maintanence.blade.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Leyla'dan Sonra</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        /*
+        * Globals
+        */
+
+        /* Links */
+        a,
+        a:focus,
+        a:hover {
+            color: #fff;
+        }
+
+        .btn-outline-light:hover {
+            color: #339999;
+        }
+
+        /*
+        * Base structure
+        */
+
+        html,
+        body {
+            height: 100%;
+            background-color: #339999;
+        }
+
+        body {
+            display: -ms-flexbox;
+            display: flex;
+            color: #fff;
+            text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+            box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+        }
+
+        .cover-container {
+            max-width: 42em;
+        }
+
+
+        /*
+        * Header
+        */
+        .masthead {
+            margin-bottom: 2rem;
+        }
+
+        .masthead-brand {
+            margin-bottom: 0;
+        }
+
+
+        @media (min-width: 48em) {
+            .masthead-brand {
+                float: left;
+            }
+            .nav-masthead {
+                float: right;
+            }
+        }
+
+
+        /*
+        * Cover
+        */
+        .cover {
+            padding: 0 1.5rem;
+        }
+        .cover .btn-lg {
+            padding: .75rem 1.25rem;
+            font-weight: 700;
+        }
+
+        h1 {
+            font-size: 48px;
+        }
+
+        img {
+            width: 300px;
+        }
+
+        @media (max-width: 768px) {
+            img {
+                width: 200px;
+            }
+            p.lead {
+                font-size: 1rem;
+            }
+
+        }
+
+        /*
+        * Footer
+        */
+        .mastfoot {
+            color: rgba(255, 255, 255, .5);
+        }
+
+    </style>
+</head>
+
+<body class="text-center">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <main role="main" class="inner cover">
+        <img class="mt-5" src="/images/front/logo@2x.png" alt="Leyla'dan Sonra">
+        <h1 class="cover-heading mt-3">Bakımdayız</h1>
+        <p class="lead">Miniklerimizin dilekleri sizlere daha iyi iletebilmek için internet sitemizi yeniliyoruz.</p>
+        <div>
+            <a href="https://www.facebook.com/leyladansonra" class="btn btn-outline-light">Facebook</a>
+            <a href="https://twitter.com/leyladansonra" class="btn btn-outline-light">Twitter</a>
+            <a href="https://www.instagram.com/leyladansonra/" class="btn btn-outline-light">Instagram</a>
+            <a href="https://www.youtube.com/leyladansonra" class="btn btn-outline-light">Youtube</a>
+        </div>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+    </footer>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js""></script>
+</body>
+</html>
