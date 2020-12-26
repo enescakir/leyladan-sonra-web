62c5c10a88dfeb7e171a67ffa1983d2e18714a39
Enes Çakır <enes@cakir.web.tr>
2020-12-27 00:57:16 +0300

    Rename pull script to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+VERSION="$1"
+
+if [ -z "$VERSION" ]; then
+    echo "Version is empty"
+    exit 1
+fi
+
 echo "Deployment started"
+echo "Version: v$VERSION"
 
 git pull
 echo "Pulled"
 
+git fetch --all --tags
+echo "Fetched"
+
+git checkout "$VERSION"
+echo "Checkout"
+
+sed -i "s%^APP_VERSION=.*$%APP_VERSION=$VERSION%" .env
+echo "App version updated"
+
 composer install --no-interaction --no-dev --prefer-dist --optimize-autoloader
 echo "Composer installed"
 
-php artisan migrate
+php artisan migrate --force --no-interaction
 echo "Migrated"
 
 php artisan view:clear
@@ -25,4 +42,4 @@ echo "Routes cached"
 php artisan queue:restart --quiet
 echo "Queue restarted"
 
-echo "Finished"
+echo "Deployed"
